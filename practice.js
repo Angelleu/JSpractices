231bb2daf19f0195d3b2f90cc1344891a4c10ec7
Angel Leu <angel840816@gmail.com>
2020-01-28 15:32:10 -0800

    add practice to git

---
+++ practice.js
@@ -0,0 +1,20 @@
+/*
+\n New Line
+\t Tab
+\r Carriage Return
+\' Single quote
+\" Double quote
+\\ Backslash
+*/
+
+
+let message = 'Escaping backslash \\ as string content';
+console.log(message); // we show \ as string content
+
+message = 'I love to have coffee at Gianu\'s';
+
+console.log(message);
+
+message = "Jets are \"the\" best NHL team";
+
+console.log(message);
