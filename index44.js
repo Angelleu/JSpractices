19c4f520eaf3573f08f6d9f4fa62fcddea68535c
Angel Leu <angel840816@gmail.com>
2020-01-28 19:03:21 -0800

    added ex44 to git

---
+++ index44.js
@@ -0,0 +1,8 @@
+const text ='HELLO';
+const result1 = text.charAt(4);
+const result2 = text.charAt(3);
+const result3 = text.charAt(2);
+const result4 = text.charAt(1);
+const result5 = text.charAt(0);
+const finalResult =result1.concat(result2,result3,result4,result5);
+console.log(`the final result is: ${finalResult.toLowerCase()}`);
