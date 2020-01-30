487429cf46f198ffa0c2c9609db2abf483afbf39
Angel Leu <angel840816@gmail.com>
2020-01-29 18:22:04 -0800

    updated ex48 to git

--- index48.js
+++ index48.js
@@ -6,3 +6,13 @@ console.log(answerToLife);
 let result = pi+answerToLife;
 console.log(`pi:${pi} \nanswerToLife:${answerToLife}`);
 console.log(result.toString(),'is the result of adding pi and answerToLife');
+
+//different way
+//using split method to make message into an Array.
+//
+//message = message.split(' ');
+//console.log(message);
+//const pi = parseFloat(message[0]);
+//console.log(pi);
+//let answerToLife = parseInt (message[6]);
+//console.log(answerToLife);
