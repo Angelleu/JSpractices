07b454a9c828bff81d1f3fb50ac1b3dbbc448ef4
Angel Leu <angel840816@gmail.com>
2020-01-29 18:01:56 -0800

    added ex55 to git

---
+++ index55.js
@@ -0,0 +1,4 @@
+let firstNumber = 10;
+let secondNumber =4;
+let message = (firstNumber>secondNumber)?'First Number is bigger than Second Number ':'Second Number is bigger than First Number';
+    console.log(message);
