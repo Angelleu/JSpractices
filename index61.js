5455afc19d79bb3a018a0a6e17fa2376863d85ae
Angel Leu <angel840816@gmail.com>
2020-01-30 18:22:44 -0800

    added ex 61 to git

---
+++ index61.js
@@ -0,0 +1,46 @@
+let weekdayNumber =5
+let message=null;
+switch (weekdayNumber){
+    case 1:
+        message='Today it\'s Monday';
+        break;
+        case 2:
+            message='Today it\'s Tuesday';
+            break;
+        case 3:
+        message='Today it\'s Wednesday';
+        break;
+        case 4:
+        message='Today it\'s Thursday';
+        break;
+        case 5:
+        message='Today it\'s Friday';
+        break;
+        case 6:
+        message='Today it\'s Saturday';
+        break;
+        case 7:
+        message='Today it\'s Sunday';
+        break;
+        default:
+        message='Error: Please input a number bettwen 1 and 7';
+    
+}console.log(message);
+/*let weekdayNumber =6;
+if (weekdayNumber===1){
+    console.log(`Today is Monday`);
+}else if(weekdayNumber===2){
+    console.log(`Today is Tuesday`);
+}else if(weekdayNumber===3){
+    console.log(`Today is Wednesday`);
+}else if(weekdayNumber===4){
+    console.log(`Today is Thursday`);
+}else if(weekdayNumber===5){
+    console.log(`Today is Friday`);
+}else if(weekdayNumber===6){
+    console.log(`Today is Saturday`);
+}else if(weekdayNumber===7){
+    console.log(`Today is Sunday`);
+}else{
+    console.log(`Error: Please input a number bettwen 1 and 7`);
+}*/
