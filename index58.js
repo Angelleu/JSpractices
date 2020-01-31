b463631bb6c8adc07672469b06dfd27e86f7e2e3
Angel Leu <angel840816@gmail.com>
2020-01-30 16:48:05 -0800

    added ex 58 to git

---
+++ index58.js
@@ -0,0 +1,10 @@
+let hours =1;
+if (hours>5 && hours<=12){
+    console.log(`Good morning`);
+}else if (hours>12 && hours<=18){
+    console.log(`Good Afternoon`);
+}else if (hours>18 && hours<=22){
+    console.log(`Good evening`);
+}else{
+    console.log(`Good night`);
+}
