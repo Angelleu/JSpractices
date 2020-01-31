e03d450e0371a0ccc9130dfa404b533cb98999dd
Angel Leu <angel840816@gmail.com>
2020-01-30 16:46:27 -0800

    updated ex 28 to git

--- index28.js
+++ index28.js
@@ -9,3 +9,8 @@ console.log(`9 times 7 equals ${number*7}`);
 console.log(`9 times 8 equals ${number*8}`);
 console.log(`9 times 9 equals ${number*9}`);
 console.log(`9 times 10 equals ${number*10}`);
+
+/*const multiplier = 9;
+for (var i = 1; i < 11; i++) {
+    console.log(`${multiplier} times ${i} equals: ${i * multiplier}`);
+}*/
