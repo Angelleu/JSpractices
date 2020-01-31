c5f323ba5b76f535860a8ab3728a69a26b107170
Angel Leu <angel840816@gmail.com>
2020-01-30 16:47:28 -0800

    add ex 59 to git

---
+++ index59.js
@@ -0,0 +1,9 @@
+let firstNumber =10;
+let secondNumber =10;
+if (firstNumber===secondNumber){
+    console.log(`They are the same number`);
+}else if(firstNumber>=secondNumber){
+    console.log(`${firstNumber} is bigger than ${secondNumber}`);
+}else{
+    console.log(`${secondNumber} is bigger than ${firstNumber}`);
+}
