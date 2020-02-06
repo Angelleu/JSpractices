5230d6ea3bb32718d45bb44975c810654564f363
Angel Leu <angel840816@gmail.com>
2020-02-06 15:52:58 -0800

    added ex101 to git

---
+++ index101.js
@@ -0,0 +1,12 @@
+const user={
+    name:'Angel',
+    age:24,
+    phone:88888888,
+    street:'w Broadway',
+    zipcode:'v8r 6h8',
+    married:false
+}
+function showUser(user){
+  console.log(user);
+}
+showUser(user);
