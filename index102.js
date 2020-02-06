3bf928f6fb01c039a13f9d36dc2f07693fa7361f
Angel Leu <angel840816@gmail.com>
2020-02-06 15:53:56 -0800

    added ex 102 to git

---
+++ index102.js
@@ -0,0 +1,8 @@
+var name="";
+var showName=function(name){
+    console.log(`===========`);
+    console.log(`= ${name} =`);
+    console.log(`===========`);
+}
+showName('Angel');
+showName('Leslie');
