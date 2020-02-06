26756bbad9a19673b882874d3a27198eb63e884b
Angel Leu <angel840816@gmail.com>
2020-02-06 15:55:05 -0800

    added ex 104 to git

---
+++ index104.js
@@ -0,0 +1,16 @@
+var fibonacci=function(){
+    let f=0;
+    let f1=-1;
+    let f2=1;
+    var i=1;
+    do{f=f1+f2;
+        f1=f2;
+        f2=f;
+        i++;
+        console.log(f);
+    }
+    while(i<=10)
+}
+fibonacci();
+fibonacci();
+fibonacci();
