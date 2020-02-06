57c2b8c1e7fbdc15e40a747578f53222fcd35c65
Angel Leu <angel840816@gmail.com>
2020-02-06 15:54:35 -0800

    added ex 103 to git

---
+++ index103.js
@@ -0,0 +1,17 @@
+var evenNumbers=function(){
+    for(let i=0; i<100;i++){
+        if(i%2==0){
+            console.log(i);
+        }
+    }
+}
+console.log(`1-------------`);
+evenNumbers();
+console.log(`2-------------`);
+evenNumbers();
+console.log(`3-------------`);
+evenNumbers();
+console.log(`4-------------`);
+evenNumbers();
+console.log(`5-------------`);
+evenNumbers();
