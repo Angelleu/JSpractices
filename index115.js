7f128343d0eb7c98b20ab641249a7dab0ef56a66
Angel Leu <angel840816@gmail.com>
2020-02-10 20:13:04 -0800

    added ex 115 to git

---
+++ index115.js
@@ -0,0 +1,14 @@
+const getLongerText=function(text1,text2){
+    /*if(text1.length>text2.length){
+        return text1
+    }else{
+        return text2
+    }
+    */
+    return text1.length>text2.length ? text1 : text2;
+    
+}
+console.log(getLongerText('sweetijfskde','catabnfrr'));
+console.log(getLongerText('sweetifodsjfskde','cnfrr'));
+console.log(getLongerText('ekje','catabnfrr'));
+console.log(getLongerText('swfldkflsm.,vde','catabnffsefrr'));
