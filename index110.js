c7e80506007df651227ff0aea2e7e40e7e04954a
Angel Leu <angel840816@gmail.com>
2020-02-10 16:41:12 -0800

    added ex 110 to git

---
+++ index110.js
@@ -0,0 +1,7 @@
+const average =function(number1,number2,number3,number4,number5){
+     var average = (number1+number2+number3+number4+number5)/5;
+    console.log('The average is '+average);
+
+}
+average(2,4,10,20,32);
+average(2,45,10,25,70);
