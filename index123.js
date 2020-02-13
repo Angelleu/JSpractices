7fb47f6c5d1880b87f1b49c21c2972337ef5cfb2
Angel Leu <angel840816@gmail.com>
2020-02-13 14:26:13 -0800

    added ex 123 to git

---
+++ index123.js
@@ -0,0 +1,3 @@
+const students=['Angel','Ken','Ray','Leslie','Lena','Rainie'];
+console.log(students[students.length-5],students[students.length-1]);
+console.log(`The students array has ${students.length=[8]} items`);
