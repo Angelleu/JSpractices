240993a6633f1f70b1ac1b6251f3c10013262b97
Angel Leu <angel840816@gmail.com>
2020-02-13 14:26:37 -0800

    added ex 124 to git

---
+++ index124.js
@@ -0,0 +1,4 @@
+const movies=['Harry Potter',
+'Love Rose','Last Christmas','The glass castle','Gretal and Hansel'
+,'Captain America','Iron Man','1917','Sonic','Fantasy island','Parasite'];
+console.log('First Movie: ',movies[0],' last movie: ',movies[movies.length-1]);
