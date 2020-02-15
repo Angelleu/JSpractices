ade30e5fc43b775a4a8c3c5a6c923a6d08c2cbd6
Angel Leu <angel840816@gmail.com>
2020-02-14 16:37:44 -0800

    added ex 125 to git

---
+++ index125.js
@@ -0,0 +1,5 @@
+const iceCreamFlavors=['Dark chocolate','Milk chocolate',
+'Cherry','Raspberry','Straeberry','Green tea'];
+iceCreamFlavors.push('Pistachio','Blue Moon','Vanilla','Milk');
+console.log(iceCreamFlavors);
+console.log(iceCreamFlavors.length);
