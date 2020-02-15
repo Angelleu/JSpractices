ab7f0283a2354cda36f204dfb87bdba6e90e536b
Angel Leu <angel840816@gmail.com>
2020-02-14 16:39:38 -0800

    added ex 126 to git

---
+++ index126.js
@@ -0,0 +1,5 @@
+const iceCreamFlavors=['Dark chocolate','Milk chocolate',
+'Cherry','Raspberry','Strawberry','Green tea'];
+iceCreamFlavors.unshift('Pistachio','Blue Moon','Vanilla','Milk');
+console.log(iceCreamFlavors);
+console.log(iceCreamFlavors.length);
