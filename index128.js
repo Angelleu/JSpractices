c671cfe4c5746a8753db2a4e6367d60d236578cb
Angel Leu <angel840816@gmail.com>
2020-02-14 16:40:39 -0800

    added ex 128 to git

---
+++ index128.js
@@ -0,0 +1,15 @@
+const iceCreamFlavors=['Dark chocolate','Milk chocolate',
+'Cherry','Raspberry','Straeberry','Green tea','Pistachio','Blue Moon','Vanilla','Milk'];
+const milk=iceCreamFlavors.pop();
+const vanilla=iceCreamFlavors.pop();
+const blueMoon=iceCreamFlavors.pop();
+const pistachio=iceCreamFlavors.pop();
+
+console.log(iceCreamFlavors);
+console.log(iceCreamFlavors.length);
+console.log('Removed item:',milk);
+console.log('Removed item:',vanilla);
+console.log('Removed item:',blueMoon);
+console.log('Removed item:',pistachio);
+
+
