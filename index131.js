53c69552f9564f6ecf078b58e0b7f4719383ba8d
Angel Leu <angel840816@gmail.com>
2020-02-14 16:41:26 -0800

    added ex 131 to git

---
+++ index131.js
@@ -0,0 +1,6 @@
+const femaleStudents=['Angel','Aurora','Jackie','Sunny','Anna'];
+const maleStudents=['Jack','Frank','Tony','Steve','Henry'];
+const Students=['Angel','Aurora','Jackie','Sunny','Anna','Jack','Frank','Tony','Steve','Henry'];
+
+console.log(Students.sort());
+console.log(femaleStudents.indexOf('Jackie'));
