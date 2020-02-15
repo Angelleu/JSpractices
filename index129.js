cf3a4417468635a1a3379ddac3f0b30767d372e6
Angel Leu <angel840816@gmail.com>
2020-02-14 16:41:04 -0800

    added index 129 to git

---
+++ index129.js
@@ -0,0 +1,9 @@
+const iceCreamFlavors=['Dark chocolate','Milk chocolate',
+'Cherry','Raspberry','Straeberry','Green tea','Pistachio','Blue Moon','Vanilla','Milk'];
+for(var i=0;i<iceCreamFlavors.length;i++){
+    console.log(iceCreamFlavors[i]);
+}
+
+
+
+
