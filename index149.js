3821594f7a04884f96a5ae80760ce910399d59d4
Angel Leu <angel840816@gmail.com>
2020-02-17 22:36:35 -0800

    added ex 149 to git

---
+++ index149.js
@@ -0,0 +1,28 @@
+const user={
+    name:'Angelbabe',
+    lastName:'Leu',
+    age:18,
+    hobby:['Movie','music','swim'],
+    married:false,
+    fruit:'strawberry',
+    drinkAlcohol:true,
+    love:'male',
+    city:'Vancouver',
+    object:{}
+}
+const myObject={
+    name:'Angelbabe',
+    lastName:'Leu',
+    age:18,
+    hobby:['Movie','music','swim'],
+    married:false,
+}
+myObject.name='Angel';
+myObject.lastName='Tu';
+myObject.age=25;
+myObject.hobby=['eat','dance','jogging'];
+myObject.married=true;
+
+console.log('user:',user);
+console.log('----------------------');
+console.log('myObject:',myObject);
