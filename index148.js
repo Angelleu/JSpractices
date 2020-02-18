e88a623dfbaa010217ad64635e095e4423944627
Angel Leu <angel840816@gmail.com>
2020-02-17 22:36:15 -0800

    added ex 148 to git

---
+++ index148.js
@@ -0,0 +1,16 @@
+const superhero={
+    name:'batman',
+    secretName:'Bruce',
+    sidekick:['Robin',
+        'Alfred',
+        'Gordon'],
+    strength:70,
+}
+
+console.log(superhero.name,
+    superhero.secretName,
+    superhero.sidekick,
+    superhero.strength);
+console.log('--------------------------');
+superhero.speed=80;
+console.log(superhero);
