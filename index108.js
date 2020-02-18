4df67d9f449eb036cfefbd1bb6d4d036f2840473
Angel Leu <angel840816@gmail.com>
2020-02-17 16:20:38 -0800

    updated ex 108 to git

--- index108.js
+++ index108.js
@@ -1,3 +1,7 @@
-const sort=function(number1,number2,number3){
-
+const sort=function(number1,number2,number3,highToLow){
+    let arr =[number1,number2,number3]
+    arr.sort((a,b) => highToLow ? b-a: a-b);
+    return arr;
 }
+console.log(sort(10,8,25));
+console.log(sort(10,8,25,true));
