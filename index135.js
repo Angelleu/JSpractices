4a87cef7a74937b8e858e5904764adc76625ac50
Angel Leu <angel840816@gmail.com>
2020-02-17 16:22:24 -0800

    added ex 135 to git

---
+++ index135.js
@@ -0,0 +1,13 @@
+const mutants=['Professor X','Cyclops','Iceman',
+'Angel','Magneto','Beast','Phoenix','Logan','Gambit'];
+mutants.forEach(function(mutant,index){
+    console.log('Index ',index);
+    console.log(mutant);
+});
+
+
+/*const pets = ['Amelia', 'Ciro', 'Ulises', 'Carlos'];
+
+pets.forEach(function(petName) {
+  console.log(petName);
+});*/
